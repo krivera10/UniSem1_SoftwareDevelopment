6a79ffd6bf08d994b53192bdc503a2417d245dd6
krivera10 <163644876+krivera10@users.noreply.github.com>
2024-03-17 00:34:28 +1100

    prac 2-task2-exercise2.2

---
+++ math-equation.py
@@ -0,0 +1,13 @@
+#The equation below must be solved by this python script
+# y= 4x + 10
+#Since X is not defined in the question I will assign "5" as the value to X
+
+X=5
+
+y = 4* X + 10
+
+#To get the answer we will print y
+
+print(y)
+
+
