979c101f474a50db07a857afb4139d4db0bea965
krivera10 <163644876+krivera10@users.noreply.github.com>
2024-03-16 22:38:21 +1100

    Hello-World

---
+++ main.py
@@ -0,0 +1,9 @@
+def print_hello_world(name):
+    print("Hi, {0}".format(name))
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hello_world('Hello World')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
